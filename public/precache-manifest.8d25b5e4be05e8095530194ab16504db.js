0285754f134aa58ae4386f29ffecc623893438c5
Jason Matthews <jmatty1983@gmail.com>
2019-04-30 19:15:41 -0400

    Configure websocket to work on heroku's environment.
    This commit also adds the front end assets from the react build. In a normal environment these would either be served from a serparate cdn or app or placed via the build process

---
+++ public/precache-manifest.8d25b5e4be05e8095530194ab16504db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b949c8fc45f2a9e0f98adc5b15fb7fae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba05f829439fd633fb13",
+    "url": "/static/css/main.16c2be75.chunk.css"
+  },
+  {
+    "revision": "46518f7449ab70f3ca6c",
+    "url": "/static/js/2.55b90362.chunk.js"
+  },
+  {
+    "revision": "ba05f829439fd633fb13",
+    "url": "/static/js/main.39f82260.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import bodyParser from 'body-parser';
 import conf from 'dotenv-safe';
 import cors from 'cors';
 import express from 'express';
+import http from 'http';
 import mongoose from 'mongoose';
 import path from 'path';
 
@@ -12,18 +13,19 @@ import { WebsocketService } from './services';
 
 const { PORT, NODE_ENV, DB_USER, DB_PASS, DB_URL, DB_NAME } = process.env;
 
-const server = express();
+const app = express();
+const server = http.createServer(app);
 
 WebsocketService.start(server);
 mongoose.connect(`mongodb://${DB_USER}:${DB_PASS}@${DB_URL}/${DB_NAME}`, { useNewUrlParser: true, autoReconnect: true });
 
 //Prod will serve assets from the public dir and have the same origin. This is just for a convenient development experience
 if (NODE_ENV === 'developement') {
-  server.use(cors());
+  app.use(cors());
 }
-server.use(express.static(path.resolve(__dirname, '..', 'public')));
-server.use(bodyParser.json());
+app.use(express.static(path.resolve(__dirname, '..', 'public')));
+app.use(bodyParser.json());
 
-server.use('/factory', FactoryRouter);
+app.use('/factory', FactoryRouter);
 
 server.listen(PORT, () => console.log(`listening on port ${PORT}.`));
